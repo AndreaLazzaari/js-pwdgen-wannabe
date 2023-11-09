b5b2e135c3a6c8bd188fd1de1b5a371d4a78714a
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-09 15:04:38 +0100

    inserito script.js in idex.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 
 // dichiarazione e assegnazione mie variabili
 
-const nomeUtente = prompt ("Inserisci il tuo nome");
-const cognomeUtente = prompt ("Inserisci il tuo cognome");
-const coloreUtente = prompt ("Inserisci il tuo colore preferito");
+const nomeUtente = prompt ('Inserisci il tuo nome');
+const cognomeUtente = prompt ('Inserisci il tuo cognome');
+const coloreUtente = prompt ('Inserisci il tuo colore preferito');
+
+console.log (nomeUtente)
+
+
 
 const risultatoEsercizio = nomeUtente + cognomeUtente + coloreUtente;
