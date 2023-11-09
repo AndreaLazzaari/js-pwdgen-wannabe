3c8c5559e49cd60e61e1f38b8a8113342def9b04
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-09 14:58:21 +0100

    inizializzazione index.html e script.js con aggiunta in index un h1, h2 e un p con un id + in script inseriti 4 const

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// dichiarazione e assegnazione mie variabili
+
+const nomeUtente = prompt ("Inserisci il tuo nome");
+const cognomeUtente = prompt ("Inserisci il tuo cognome");
+const coloreUtente = prompt ("Inserisci il tuo colore preferito");
+
+const risultatoEsercizio = nomeUtente + cognomeUtente + coloreUtente;
